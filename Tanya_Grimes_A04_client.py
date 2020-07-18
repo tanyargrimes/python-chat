f39f3f296d388cba34a6ddb33d315f8b105a8f7e
Tanya Grimes <tanyargrimes@gmail.com>
2020-07-17 21:00:18 -0400

    Added input validations, instructions and formatting

--- Tanya_Grimes_A04_client.py
+++ Tanya_Grimes_A04_client.py
@@ -1,6 +1,10 @@
 # !/usr/local/bin/python
 # -*- coding: utf-8 -*-
-"""
+'''
+PROG8420 - Programming for Big Data
+
+Assignment 04 Client
+
 Created on Fri Jul 10 09:18:54 2020
 
 @authors: 
@@ -12,54 +16,126 @@ Code modified from:
 -    https://medium.com/swlh/lets-write-a-chat-app-in-python-f6783a9ac170
 -    https://realpython.com/python-gui-tkinter/
 -    http://effbot.org/zone/tkinter-scrollbar-patterns.htm#:~:text=to%20standard%20widgets.-,The%20Scrollbar%20Interface,the%20Text%20widget.
-"""
+'''
 
 
 #------------------------------------------
 # Library Imports
 
-from socket import AF_INET, socket, SOCK_STREAM, gethostname, gethostbyname
+from socket import AF_INET, socket, SOCK_STREAM
 from threading import Thread
 import tkinter as tk
 
 
 #------------------------------------------
-# Constant Definitions
-
-# will be an input once more
-#SHOST = '192.168.0.15'
-#SHOST = '172.18.64.129' # Ethernet IP for server
-SHOST = '192.168.0.11' #Wifi IP for server
-#SHOST = '72.137.51.162' # Public IP
-#SHOST = '0.0.0.0'
-SPORT = 33000
-#CHOST = gethostname()
-CHOST = '192.168.0.11' # Wifi IP for client
-#CHOST = '172.18.64.129' # Ethernet for client
-CPORT = 12348
-BUFSIZ = 1024
-SADDR = (SHOST, SPORT)
-CADDR = (CHOST, CPORT)
-print('Address:', SADDR, CADDR)
-
-
-#------------------------------------------
 # Function Definitions
 
-def receive():
-    """ Handles receiving of messages """
+def validate_ipv4(address):
+    ''' validates host input and prints message if invalid '''
+    
+    is_valid = True
+    
+    if address == '' or address == '0.0.0.0':
+        print('IP address format is not valid for the client setup.')
+        is_valid = False
+    elif (len(address) > 0 and address.count('.') != 3):
+        # a valid ipv4 address should contain 3 dots
+        print('IP address is not valid.')
+        print('The format should contain 3 dots for wiFi. E.g. 158.12.0.13')
+        is_valid = False
+    
+    elif len(address) > 0 and address.count('.') == 3:
+        # validates values between the dots
+        
+        ip_split = address.split('.')
+        for i in range(len(ip_split)):
+            if ip_split[i] == '':
+                print('IP address is not valid.')
+                print('Format is missing numbers between dots')
+                is_valid = False
+                break
+            elif not ip_split[i].isnumeric():
+                print('IP address is not valid.')
+                print('Only numbers are allowed between dots')
+                is_valid = False
+                break
+            elif ip_split[i][0] == '0' and len(ip_split[i]) > 1:
+                print('IP address is not valid.')
+                print('Please do not use leading 0s')
+                is_valid = False
+                break
+            elif not(int(ip_split[i]) > -1 and int(ip_split[i]) < 256):
+                print('IP address is not valid.')
+                print('Each number should be between 0 and 255 inclusive')
+                is_valid = False
+                break
+        
+        # returns true if no errors were found in the looping conditions
+        return is_valid
+    
+    else:
+        return True
+    
+    # returns true if no errors were found
+    return is_valid
+
+
+def retrieve_address(type):
+    ''' recursively validates the input and only returns when valid '''
+    
+    host_input = input('Please enter the ' + type + ' IP address: ').strip()
+        
+    if validate_ipv4(host_input):
+        return host_input
+    else:
+        return retrieve_address(type)
+ 
+
+def validate_port(port, s_port, type):
+    ''' validates port input and prints message if invalid '''
+    
+    if port == '':
+        print('Port number is required.')
+    elif not port.isnumeric():
+        print('Port number is not valid.')
+        print('Only numbers are allowed.')
+    elif not(int(port) > 0 and int(port) < 65536):
+        print('Port number is not valid.')
+        print('Number should be between 1 and 65535 inclusive.')
+    elif s_port != None and int(port) == s_port and type.lower() == 'client':
+        print('Port number is not valid.')
+        print('The Client cannot share the same port as the Server.')
+    else:
+        return True
+    
+    # return false if errors were found
+    return False
+        
+ 
+def retrieve_port(type, s_port = None):
+    ''' recursively validates the input and only returns when valid '''
+    
+    port_input = input('Please enter the ' + type + ' port number: ').strip()
+    
+    # check conditions for a valid port number
+    if validate_port(port_input, s_port, type):
+        return int(port_input)
+    else:
+        return retrieve_port(type, s_port)
+    
+
+def receive_message():
+    ''' Handles receipt of message '''
+    
     while True:
         try:
-            msg = client_socket.recv(BUFSIZ).decode("utf8")
-            print(msg)
-            # if msg[0:2] == 'c-':
-            #     lbl_connum.set('Active: ' + msg[2:])
-            # else:
-            # add messages to end of the list
-            lst_messages.insert(tk.END, msg)
+            receiving_msg = client_socket.recv(BUFFER_SIZE).decode("utf8")
+            
+            # adds message to chat list box
+            lst_messages.insert(tk.END, receiving_msg)
             lst_messages.insert(tk.END, '')
             
-            # set the vertical view to see the end of the list
+            # sets the vertical view to see the end of the list
             lst_messages.yview(tk.END)
             
         except OSError:  
@@ -67,13 +143,11 @@ def receive():
             break
 
 
-def send(event = None):
-    """ Displays the current message and sends to server """
+def send_message(event = None):
+    ''' Displays the current message and sends to the server '''
     
     # stores the current input text
     msg_raw = msg_current.get()
-    
-    print(msg_raw)
     
     # clears the input box for re-use
     msg_current.set('')
@@ -88,62 +162,31 @@ def send(event = None):
 
 
 def on_closing(event = None):
-    """ Executes on closing of the widget """
+    ''' Prepares for the closing the widget closing 
+    by passing appropriate exit messages '''
+    
     msg_current.set('{x}')
-    send()
+    send_message()
 
 
 #------------------------------------------
 # Tkinter GUI Definitions
 
+TK_THEME_COL = 'green'
+TK_BTN_TXT_COL = 'white'
+TK_FRM_BG_COL = '#eee'
+
 gui_client = tk.Tk()
-gui_client.title("Chatter Box")
-
-# frm_title = tk.Frame(
-#     master = gui_client,
-#     relief = tk.FLAT
-# )
-# frm_title.pack(
-#     fill = tk.BOTH
-# )
-
-# lbl_greeting = tk.Label(
-#     master = frm_title,
-#     text = 'Welcome to Chatter Box!'.upper(),
-#     fg = 'green'
-# )
-# lbl_greeting.pack(
-#     side = tk.LEFT,
-#     fill = tk.BOTH,
-#     anchor = tk.W,
-#     expand = True
-# )
-
-# # stores label text as a string variable
-# lbl_connum = tk.StringVar()
-
-# lbl_connections = tk.Label(
-#      master = frm_title,
-#     text = '',
-#     width = 10,
-#     fg = 'green',
-#     padx = 30,
-#     textvariable = lbl_connum
-# )
-# lbl_connections.pack(
-#     side = tk.RIGHT,
-#     fill = tk.Y,
-#     anchor = tk.E,
-#     expand = True
-# )
+gui_client.title('Chatter Box')
 
 lbl_greeting = tk.Label(
     text = 'Welcome to Chatter Box!'.upper(),
-    fg = 'green',
+    fg = TK_THEME_COL,
     width = 70,
 )
 lbl_greeting.pack()
 
+# adds frame to hold the list box and scrollbar
 frm_messages = tk.Frame(
     master = gui_client,
     relief = tk.FLAT,
@@ -165,7 +208,7 @@ srl_scrollbar = tk.Scrollbar(
 lst_messages = tk.Listbox(
     master = frm_messages,
     height = 20,
-    bg = '#eee',
+    bg = TK_FRM_BG_COL,
     yscrollcommand = srl_scrollbar.set
 )
 lst_messages.pack(
@@ -174,7 +217,7 @@ lst_messages.pack(
     expand = True
 )
 
-# add extra config after listbox is defined and load
+# adds extra config to scrollbar after listbox is defined and loaded
 srl_scrollbar.config(command = lst_messages.yview)
 srl_scrollbar.pack(
     side = tk.RIGHT,
@@ -184,46 +227,92 @@ srl_scrollbar.pack(
 # stores input as a string variable
 msg_current = tk.StringVar()
 
-# add the input box
+# adds the input box
 input_box = tk.Entry(
     width = 80,
     textvariable = msg_current
 )
-input_box.bind("<Return>", send)
+# binds return key to input with send_message call when pressed
+input_box.bind("<Return>", send_message)
 input_box.pack(
     side = tk.LEFT,
     fill = tk.BOTH,
     expand = True
 )
 
-# add send button
+# adds send button with send_message call on button click
 btn_send = tk.Button(
-    text='Send',
+    text = 'Send',
     width = 9,
     height = 2,
-    bg = 'green',
-    fg = 'white',
-    command = send
+    bg = TK_THEME_COL,
+    fg = TK_BTN_TXT_COL,
+    command = send_message
 )
 btn_send.pack(
     side = tk.RIGHT
 )
 
-# calls to on_closing function when the widget X button is clicked
-gui_client.protocol("WM_DELETE_WINDOW", on_closing)
+# calls to the on_closing function when the widget X button is clicked
+# this seems to work on Windows 10, but will display forcibly closed connection
+# message on the server console.
+gui_client.protocol('WM_DELETE_WINDOW', on_closing)
 
 
-#------------------------------------------
-# Open Client Widget
+#--------------------------------------------------------------
+# Variable / Constant Definitions & Client to Server Connection
+
+print('\n-------------------------------------------------------')
+print('* Python Socket Chat - Client Setup\n')
+print('* This application has been set up to allow the')
+print('  server and client(s) to communicate once they')
+print('  are all on the same LAN WiFi network.\n')
+print('* It is limited to one client per machine.\n')
+print('* The server must be run first.\n')
+print('* The server IP address and port number are required.')
+print('  For convenience this information is displayed')
+print('  after the server has started successfully')
+print('  and can be used to fill the prompts here.\n')
+print('* There is no custom check for the correct server WiFi IP address,')
+print('  so a built-in error message will not be seen until all')
+print('  prompts have been filled in successfully.\n')
+print('* It is the same for a correct client WiFi IP address.\n')
+print('* If a built-in error is thrown, please open a new Python console')
+print('  and close the previous one to kill any lingering connections.\n')
+print('* If the server address was left empty or has the format:')
+print('  0.0.0.0 on the server setup, the server address to be entered')
+print('  on the client setup must be the actual LAN WiFi IP address.\n')
+print('* Empty string and 0.0.0.0 server formats were allowed for quick')
+print('  testing between a client and server on the same machine only.\n')
+print('* The client IP address and port number are also required')
+print('  to verify the client is on the same LAN WiFi network,')
+print('  especially if it is not on the same machine as the server.\n')
+print('* It is assumed IPv4 addresses will be used.\n')
+print('* It is assumed only one server will be running.\n')
+print('* It is assumed all ports entered are available.')
+print('-------------------------------------------------------\n')
+
+S_HOST = retrieve_address('Server')
+S_PORT = retrieve_port('Server')
+C_HOST = retrieve_address('Client')
+C_PORT = retrieve_port('Client', S_PORT)
+S_ADDRESS = (S_HOST, S_PORT)
+C_ADDRESS = (C_HOST, C_PORT)
+BUFFER_SIZE = 1024
+print('\nServer Address:', S_ADDRESS)
+print('Client Address:', C_ADDRESS)
 
 client_socket = socket(AF_INET, SOCK_STREAM)
-client_socket.bind(CADDR)
-client_socket.connect(SADDR)
 
-receive_thread = Thread(target = receive)
+# need to bind client address to verify client on a different machine is on
+# the same LAN WiFi network as the server
+client_socket.bind(C_ADDRESS)
+client_socket.connect(S_ADDRESS)
+
+receive_thread = Thread(target = receive_message)
 receive_thread.start()
 
-# listens for events to prevent another window from opening
+# opens chat widget and listens for events to prevent another window from opening
 gui_client.mainloop()
 
 
--- Tanya_Grimes_A04_server.py
+++ Tanya_Grimes_A04_server.py
@@ -1,6 +1,10 @@
 # !/usr/local/bin/python
 # -*- coding: utf-8 -*-
-"""
+'''
+PROG8420 - Programming for Big Data
+
+Assignment 04 Server
+
 Created on Fri Jul 10 09:15:30 2020
 
 @authors: 
@@ -10,14 +14,13 @@ Created on Fri Jul 10 09:15:30 2020
 
 Code modified from:
     https://medium.com/swlh/lets-write-a-chat-app-in-python-f6783a9ac170
-"""
-
+'''
 
 
 #------------------------------------------
 # Library Imports
 
-from socket import AF_INET, socket, SOCK_STREAM, gethostname, gethostbyname
+from socket import AF_INET, socket, SOCK_STREAM
 from threading import Thread
 import datetime as dt
 
@@ -25,105 +28,210 @@ import datetime as dt
 #------------------------------------------
 # Function Definitions
 
-def accept_incoming_connections():
-    """ Sets up handling for incoming clients. """
+def validate_ipv4(address):
+    ''' validates host input and prints message if invalid '''
+    
+    is_valid = True
+    
+    if len(address) > 0 and address.count('.') != 3:
+        # a valid ipv4 address should contain 3 dots
+        print('IP address is not valid.')
+        print('The format should contain 3 dots for wiFi. E.g. 158.12.0.13')
+        print('or can be empty for local machine testing with one client.')
+        is_valid = False
+        
+    elif len(address) > 0 and address.count('.') == 3:
+        # validates values between the dots
+        ip_split = address.split('.')
+        for i in range(len(ip_split)):
+            if ip_split[i] == '':
+                print('IP address is not valid.')
+                print('Format is missing numbers between dots')
+                is_valid = False
+                break
+            elif not ip_split[i].isnumeric():
+                print('IP address is not valid.')
+                print('Only numbers are allowed between dots')
+                is_valid = False
+                break
+            elif ip_split[i][0] == '0' and len(ip_split[i]) > 1:
+                print('IP address is not valid.')
+                print('Please do not use leading 0s')
+                is_valid = False
+                break
+            elif not(int(ip_split[i]) > -1 and int(ip_split[i]) < 256):
+                print('IP address is not valid.')
+                print('Each number should be between 0 and 255 inclusive')
+                is_valid = False
+                break
+        
+        # return true if no errors found in the looping conditions
+        return is_valid
+    
+    else:
+        return True
+    
+    # return false if errors have been found
+    return is_valid
+
+
+def retrieve_address():
+    ''' recursively validates the input and only returns when valid '''
+    
+    host_input = input('Please enter the Server IP address: ').strip()
+        
+    if validate_ipv4(host_input) == True:
+        return host_input
+    else:
+        return retrieve_address()
+ 
+    
+def validate_port(port):
+    if port == '':
+        print('Port number is required.')
+    elif not port.isnumeric():
+        print('Port number is not valid.')
+        print('Only numbers are allowed.')
+    elif not(int(port) > 0 and int(port) < 65536):
+        print('Port number is not valid.')
+        print('Number should be between 1 and 65535 inclusive.')
+    else:
+        return True
+    
+    # return false if errors have been found
+    return False
+
+
+def retrieve_port():
+    ''' recursively validates the input and only returns when valid '''
+    
+    port_input = input('Please enter the Server port number: ').strip()
+    
+    # check conditions for a valid port number
+    if validate_port(port_input):
+        return int(port_input)
+    else:
+        return retrieve_port()
+    
+
+def initialize_connection():
+    ''' Accepts client connection and prepares client to enter the chat '''
+    
     while True:
         client, client_address = SERVER.accept()
-        #print("%s:%s has connected." % client_address)
         client.send(bytes('Enter your name to join the chat.', 'utf8'))
+        
+        # keeps track of active client addresses
         addresses[client] = client_address
-        Thread(target = handle_client, args = (client,client_address)).start()
+        
+        # open thread to allow client to participate in the chat
+        Thread(target = manage_client, args = (client,client_address)).start()
 
 
-def handle_client(client, client_address):
-    """ Handles a single client connection. """
+def manage_client(client, client_address):
+    ''' Manages a single client connection and any client interactions '''
+    
     client_name = client_address[0]
-    user_name = (client.recv(BUFSIZ).decode("utf8")).strip()
-    #print('name on start',user_name)
+    user_name = (client.recv(BUFFER_SIZE).decode('utf8')).strip()
+    
     # provide a default name if the name is empty
     if len(user_name) == 0:
         user_name = 'Anonymous'
     
-    # set name to uppercase
-    #user_name = user_name.upper()
+    # display welcome message with client's user name
+    welcome_msg = 'Welcome %s! If you ever want to quit, type {x} to exit.' % user_name
     
-    welcome = 'Welcome %s! If you ever want to quit, type {x} to exit.' % user_name
-    
-    # sends to newly joined client only
-    client.send(bytes(welcome, "utf8"))
-    print(welcome)
+    # sends welcome message to new client only
+    client.send(bytes(welcome_msg, 'utf8'))
     
     # adds new client by name to keeps track of all active clients
     clients[client] = user_name
     
-    print('clients', clients)
-    msg = '{0} has just been connected. Total connections is {1}.'.format(client_name, len(clients))
+    # creates connection message with formatting
+    connection_msg = '{0} has just been connected. Total connections is {1}.'.format(client_name, len(clients))
     
-    broadcast(bytes(msg, 'utf8'))
+    # broadcasts connection message to all clients
+    broadcast(bytes(connection_msg, 'utf8'))
     
-    #print('wait...')
-    
-    # update active connections display
-    #broadcast(bytes('c-' + str(len(clients)), 'utf8'))
-
+    # keep listening for user input
     while True:
-        msg = client.recv(BUFSIZ)
+        broadcast_msg = client.recv(BUFFER_SIZE)
         
-        #print('msg - server', msg)
-        if msg != bytes("{x}", "utf8"):
-            # set prefix to include date and time
+        # checks if user has entered special characters to exit the chat
+        if broadcast_msg != bytes("{x}", "utf8"):
+            # sets prefix to include date and time
             dte_now = (dt.datetime.now()).strftime('%Y-%m-%d %H:%M')
             prefix = '[ ' + str(dte_now) + ' ]     ' + user_name + ':  '
-            broadcast(msg, prefix)
+            
+            # broadcasts message to all clients
+            broadcast(broadcast_msg, prefix)
         else:
             # deletes exiting client to keeps track of all active clients accurately
             del clients[client]
-            #print('clients length on close',len(clients))
-            # update active connections display
-            #broadcast(bytes('c-' + str(len(clients)), 'utf8'))
-            client.send(bytes("{x}", "utf8"))
             client.close()
             
-            broadcast(bytes("%s has left the chat." % user_name, "utf8"))
+            # broadcasts to all available clients that a user has left
+            user_exit_msg = '%s has left the chat.' % user_name
+            broadcast(bytes(user_exit_msg, 'utf8'))
             
-            msg = '{0} has disconnected. Total connections is {1}.'.format(client_name, len(clients))
-            broadcast(bytes(msg, 'utf8'))
+            # broadcasts to all available clients that client has disconnected
+            disconnection_msg = '{0} has disconnected. Total connections is {1}.'.format(client_name, len(clients))
+            broadcast(bytes(disconnection_msg, 'utf8'))
             break
 
 
-def broadcast(msg, prefix=""):
-    """Broadcasts a message to all the clients."""
-    for sock in clients:
-        # included condition to prevent certain messages from being displayed
-        # to the newly joined client
-        # if exclude_new and client == sock:
-        #     pass
-        # else:
-        sock.send(bytes(prefix, "utf8") + msg)
+def broadcast(message, prefix=''):
+    ''' Broadcasts a message to all clients, with an optional prefix. '''
+    for client in clients:
+        client.send(bytes(prefix, 'utf8') + message)
 
 
-#------------------------------------------
-# Constant & Variable Definitions
-        
+#----------------------------------------------------------
+# Variable / Constant Definitions and Server Initialization
+
+print('\n--------------------------------------------------')
+print('* Python Socket Chat - Server Setup\n')
+print('* This application has been set up to allow the')
+print('  server and client(s) to communicate once they')
+print('  are all on the same LAN WiFi network.\n')
+print('* It is limited to one client per machine.\n')
+print('* The server must be run first.\n')
+print('* If there will only be one client and it sits')
+print('  on the same machine as the server, the server')
+print('  address can be left empty or entered as: 0.0.0.0\n')
+print('* Empty string and 0.0.0.0 formats were allowed for quick')
+print('  testing between a client and server on the same machine only.\n')
+print('* If there will be at least one client on a')
+print('  different machine, please ensure it is connected')
+print('  on the same WiFi network as the server and')
+print('  use the server LAN WiFi address instead.\n')
+print('* There is no custom check for the correct server IP address,')
+print('  so a built-in error message will not be seen until all')
+print('  prompts have been filled in successfully.\n')
+print('* If a built-in error is thrown, please open a new Python console')
+print('  and close the previous one to kill any lingering connections.\n')
+print('* It is assumed IPv4 addresses will be used.\n')
+print('* It is assumed only one server will be running.')
+print('--------------------------------------------------\n')
+
+
+HOST = retrieve_address()
+PORT = retrieve_port()
+BUFFER_SIZE = 1024
+ADDRESS = (HOST, PORT)
+print('\nServer Address:', ADDRESS)
+
+# to store the active clients and addresses
 clients = {}
 addresses = {}
-#HOST = '0.0.0.0'
-#HOST = '' # accepts connections on all available IPv4 connections
-#HOST = '72.137.51.162' # Public IP
-HOST = '192.168.0.11' # Wifi IP
-#HOST = gethostbyname(gethostname()) # Ethernet IP - only works for local clients
-PORT = 33000
-BUFSIZ = 1024
-ADDR = (HOST, PORT)
-print('Address:', ADDR)
 
 SERVER = socket(AF_INET, SOCK_STREAM)
-SERVER.bind(ADDR)
+SERVER.bind(ADDRESS)
 
 if __name__ == "__main__":
     SERVER.listen(5)
     print("Waiting for connection...")
-    ACCEPT_THREAD = Thread(target = accept_incoming_connections)
+    ACCEPT_THREAD = Thread(target = initialize_connection)
     ACCEPT_THREAD.start()
     ACCEPT_THREAD.join()
     SERVER.close()
